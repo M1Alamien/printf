5a0847f40e92b528c42008e095d97288475a4208
M1Alamien <m.alamien0@gmail.com>
2023-10-14 19:22:41 +0300

    m

---
+++ printf.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * longint - name
+ * @n: int from main
+ *
+ * prints long int using _puchar
+ * Return: lenght of int
+ */
+int longint(int n)
+{
+	static int c = 0;
+	c++;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		longint(n / 10);
+
+	_puthcar((n % 10) + '0');
+	return (c);
+}
+/**
+ * _printf - name
+ * @format: string from main
+ *
+ * produces output according to a format
+ * Return: counter
+ */
+int _printf(const char *format, ...)
+{
+	int i, j, k, counter = 0;
+	char *s;
+	va_list prnt;
+
+	va_start(prnt, format);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					_putchar(va_arg(prnt, int));
+					counter++;
+					break;
+				case 'd':
+					j = va_arg(prnt, int);
+					counter += longint(j);
+					break;
+				case 's':
+					s = va_arg(prnt, char *);
+					if (s != NULL)
+					{
+						for (k = 0; s[k]; k++, counter++)
+							_putchar(s[k]);
+					}
+					break;
+				case 'i':
+					j = va_arg(prnt, int);
+					counter += longint(j);
+					break;
+				default:
+					_putchar(format[i]);
+					counter++;
+					break;
+			}
+			i++;
+		}
+		else
+		{
+			putchar(format[i]);
+			i++;
+		}
+	}
+	va_end(prnt);
+	return (counter);
+}
